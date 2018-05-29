ecdfc5d95be69d85085ddfe4738f5dab7e53ba45
Sasha Kipnis <akipnis@purdue.edu>
2018-05-29 10:29:07 -0400

    Add and style navLinks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { Route, Switch, NavLink } from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -9,9 +10,20 @@ class App extends Component {
           <h3>Ain't no party like an</h3>
           <h1>API Party</h1>
         </div>
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">Github API</NavLink>
+          </li>
+
+        </ul>
+      
+        <Switch>
+          <Route path="/github" render={() => <h1>Github!</h1>}/>
+          <Route render={() => <p>To get started, click one of the links above</p>}/>
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
