a2b7b7f6b4a1a35094186dce03702a00e0be8eab
Sasha Kipnis <akipnis@purdue.edu>
2018-05-29 10:57:15 -0400

    Add github form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import './App.css'
 import { Route, Switch, NavLink } from 'react-router-dom'
 
+import Github from './Github'
+
 class App extends Component {
   render() {
     return (
@@ -14,11 +16,19 @@ class App extends Component {
           <li>
             <NavLink to="/github">Github API</NavLink>
           </li>
+          <li>
+            <NavLink to="/nasa">NASA API</NavLink>
+          </li>
+          <li>
+            <NavLink to="/homework">Homework</NavLink>
+          </li>
 
         </ul>
       
         <Switch>
-          <Route path="/github" render={() => <h1>Github!</h1>}/>
+          <Route path="/github" render={() => <Github />}/>
+          <Route path="/nasa" render={() => <h1>NASA!</h1>}/>
+          <Route path="/homework" render={() => <h1>Homework!</h1>}/>
           <Route render={() => <p>To get started, click one of the links above</p>}/>
         </Switch>
       </div>
